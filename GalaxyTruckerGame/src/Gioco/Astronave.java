c4d64bdbe15713f8913f6da4c0c7b9c46efe0243
DiegoDalMas <d.dalmas1@studenti.unibg.it>
2025-05-08 23:39:01 +0200

    add some method in Astronave,Gioco, Giocatore

--- GalaxyTruckerGame/src/Gioco/Astronave.java
+++ GalaxyTruckerGame/src/Gioco/Astronave.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 
+
 public class Astronave { 
 	private List<Alieno> alieni;
 	private List<Batteria> batterie;
@@ -19,7 +20,7 @@ public class Astronave {
         this.griglia= new Casella[size1][size2];
 		for (int i = 0; i < size1; i++) {
 			for (int j = 0; j < size2; j++) {
-				griglia[i][j] = new Casella(i, j);
+				griglia[i][j] = new Tessera() ;
 			}
 		}
 	}

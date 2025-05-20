3f1759d39e1a47f73618df99777b7c593b9212f3
DavideMaffeis <d.maffeis10@studenti.unibg.it>
2025-05-20 16:32:31 +0200

    fatto metodo trovaPosizione per trovare la posizione del giocatore in base al colore, fatto metodo muoviGiocatore per mandare avanti/indietro il giocatore in base ai turni di volo

--- GalaxyTruckerGame/src/Gioco/PlanciaVolo.java
+++ GalaxyTruckerGame/src/Gioco/PlanciaVolo.java
@@ -14,15 +14,61 @@ public class PlanciaVolo {
 	}
 	
 	// Posiziona un giocatore in una posizione iniziale
-		public boolean posizionaGiocatore(Colore colore, int posizionePartenza) {
-			int pos = POSIZIONE_CENTRALE + posizionePartenza;
-			if(pos < 0 || pos >= DIMENSIONE) return false;
-	        if (posizioni[pos] != null) return false; 		// già occupato
-	        posizioni[pos] = colore;
-	        return true;
-	    }
+	public boolean posizionaGiocatore(Colore colore, int posizionePartenza) {
+		int pos = POSIZIONE_CENTRALE + posizionePartenza;
+		if(pos < 0 || pos >= DIMENSIONE) return false;
+        if (posizioni[pos] != null) return false; 		// già occupato
+        posizioni[pos] = colore;
+        return true;
+    }
 	
-	
+	// Muove un giocatore in avanti di n spazi
+    public boolean muoviGiocatore(Colore colore, int spazi) {
+        int posAttuale = trovaPosizione(colore);
+        if (posAttuale == -1){
+			return false;
+		}
+
+		int delta = Math.abs(spazi);
+		int nuovaPosizione = posAttuale;
+
+		if(spazi > 0){		//movimento in avanti
+			while(delta > 0){
+				nuovaPosizione++;
+				if(nuovaPosizione >= posizioni.length) return false;		//fuori dai limiti
+				if(posizioni[nuovaPosizione] == null){
+					delta--;
+				}
+			}
+		}else if (spazi < 0){		//movimento all'indietro
+			while(delta > 0){
+				nuovaPosizione--;
+				if(nuovaPosizione < 0) return false;		//fuori dal limite
+				if(posizioni[nuovaPosizione] == null){
+					delta--;
+				}
+			}
+		}else{
+			// spazi = 0 nonci muoviamo
+			return false;
+		}
+
+		// movimento
+		posizioni[posAttuale] = null;
+		posizioni[nuovaPosizione] = colore;
+		return true;
+    }
+
+
+	// metodo per trovare la posizione attuale di un giocatore in base al colore
+    public int trovaPosizione(Colore colore) {
+        for (int i=0; i<posizioni.length; i++) {
+            if (posizioni[i] == colore){
+				return i;
+			}
+        }
+        return -1;
+    }
 	
 	
 	

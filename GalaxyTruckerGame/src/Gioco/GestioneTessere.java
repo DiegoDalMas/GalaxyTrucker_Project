d9362dcf32314ad5989f19f304de53d11bc4f84a
DavideMaffeis <d.maffeis10@studenti.unibg.it>
2025-05-27 20:30:12 +0200

    modifica metodo caricatessereDaFile in class GestioneTessere per aggiungere anche le direzioni, aggiunta enum Direzione con {NORD, EST, SUD, OVEST}

--- GalaxyTruckerGame/src/Gioco/GestioneTessere.java
+++ GalaxyTruckerGame/src/Gioco/GestioneTessere.java
@@ -1,14 +1,13 @@
 package Gioco;
 import java.io.*;
-import java.nio.file.*;
 import java.util.*;
 
 
 public class GestioneTessere {
 	private List<Tessera> mazzoTessere;
 	
-	// crea mazzo delle tessere gia mischiato
-    public GestioneTessere(String path){
+	// CREA MAZZO DELLE TESSERE GIA MISCHIATO
+	public GestioneTessere(String path){
         this.mazzoTessere = new ArrayList<>();
         try {
         	this.mazzoTessere = caricaTessereDaFile(path);
@@ -18,11 +17,12 @@ public class GestioneTessere {
         }
     }
     
-    public static List<Tessera> caricaTessereDaFile(String path) throws IOException{
+	public static List<Tessera> caricaTessereDaFile(String path) throws IOException{
         List<Tessera> tessere = new ArrayList<>();
         BufferedReader br = new BufferedReader(new FileReader(path));
         String linea;
         br.readLine();      //legge la prima saltando l'indice
+
         while(  (linea = br.readLine()) != null){
             String[] campi = linea.split(",");
             TipoTessera tipo = TipoTessera.valueOf(campi[0]);
@@ -32,7 +32,13 @@ public class GestioneTessere {
             TipoConnettore sinistra = TipoConnettore.valueOf(campi[4]);
             int capacita_batterie = Integer.parseInt(campi[5]);
             int capacita_stiva = Integer.parseInt(campi[6]);
-            Tessera t = new Tessera(alto, destra, basso, sinistra, tipo, capacita_batterie, capacita_stiva);
+            EnumSet<Direzione> direzioni = EnumSet.noneOf(Direzione.class);
+            if(campi.length > 7 && !campi[7].isBlank()){
+                for(String dir: campi[7].split("-")){
+                    direzioni.add(Direzione.valueOf(dir.trim()));       //trim rimuove spazi
+                }
+            }
+            Tessera t = new Tessera(alto, destra, basso, sinistra, tipo, capacita_batterie, capacita_stiva, direzioni);
             tessere.add(t);
         }
 

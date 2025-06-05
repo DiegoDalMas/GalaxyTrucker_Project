e636214b16871f021442ff5634586aa06882274e
DavideMaffeis <d.maffeis10@studenti.unibg.it>
2025-06-05 17:36:10 +0200

    class Contrabbandieri, implementato il costruttore, cambiato attributi da Merci merci a List<Merci> merci, gestito la perdita dei giorni di volo

--- GalaxyTruckerGame/src/Gioco/Carte/Carta.java
+++ GalaxyTruckerGame/src/Gioco/Carte/Carta.java
@@ -1,9 +1,10 @@
 package Gioco.Carte;
 import java.util.List;
 import Gioco.Giocatore;
+import Gioco.PlanciaVolo;
 
 public abstract class Carta{
-    public abstract void applicaEffetto(List<Giocatore> giocatori, Giocatore leader);
+    public abstract void applicaEffetto(List<Giocatore> giocatori, Giocatore leader, PlanciaVolo plancia);
     
     
     public String getTipo() {
--- GalaxyTruckerGame/src/Gioco/Carte/Contrabbandieri.java
+++ GalaxyTruckerGame/src/Gioco/Carte/Contrabbandieri.java
@@ -4,34 +4,47 @@ import java.util.List;
 
 import Gioco.Giocatore;
 import Gioco.Merci;
+import Gioco.PlanciaVolo;
 
 public class Contrabbandieri extends Carta {
-	private boolean conferma =false;
-	private int potFuocoContrabbandieri;
+	private boolean conferma=false;
+    private int potenzaFuocoContrabbandieri;
 	private int penalita;
-	private Merci ricompensa;
-	public Contrabbandieri() {
-
-	}
+    private int giorniDiVoloPersi;
+    private List<Merci> ricompensa;
+    public Contrabbandieri(int potenzaFuoco, int penalita, int giorniDiVoloPersi, List<Merci> ricompensa){
+        this.potenzaFuocoContrabbandieri = potenzaFuoco;
+        this.penalita = penalita;
+        this.giorniDiVoloPersi = giorniDiVoloPersi;
+        this.ricompensa = ricompensa;
+    }
 
 	@Override
-	public void applicaEffetto(List<Giocatore> giocatori, Giocatore leader) {
+	public void applicaEffetto(List<Giocatore> giocatori, Giocatore leader, PlanciaVolo plancia) {
 		System.out.println("CARTA CONTRABBANDIERI");
-		do {
-			for (Giocatore g : giocatori) {
-				int potFuocoGiocatore = g.getAstronave().getPotenzaDiFuoco();
-				if (potFuocoContrabbandieri > potFuocoGiocatore) {
-					System.out.println("HAI PERSO");
+		System.out.println("POTENZA: "+potenzaFuocoContrabbandieri);
+        System.out.println("Penalita: perdi " + penalita + " merci");
+		
+		do{
+			for(Giocatore g: giocatori){
+				int potenzaFuocoGiocatore = g.getAstronave().getPotenzaDiFuoco();	//DA FARE
+				if(potenzaFuocoContrabbandieri > potenzaFuocoGiocatore){
+                	System.out.println("HAI PERSO! Perdi " + penalita + " merci");
 					g.getAstronave().perditaMerce(penalita);
-				} else if (potFuocoContrabbandieri < potFuocoGiocatore) {
-					System.out.println("HAI VINTO");
-                   g.getAstronave().assegnaMerce(ricompensa);
-                   conferma=true;
-				}else {
-					System.out.println("HAI PAREGGIATO");
+				}else if(potenzaFuocoContrabbandieri < potenzaFuocoGiocatore){
+                	System.out.println("HAI VINTO! Ottieni la ricompensa.");
+					g.getAstronave().assegnaMerce(ricompensa);
+					System.out.println("Merce ricevuta: ");
+					for(Merci m: ricompensa){
+						System.out.println(m.getColore());
+					}
+					System.out.println("Hai perso 2 giorni di volo");
+					plancia.muoviGiocatore(g.getColore(), -giorniDiVoloPersi);
+					conferma = true;
+				}else{
+                	System.out.println("PAREGGIO: i contrabbandieri passano al prossimo giocatore.");
 				}
 			}
-		} while (conferma == false);
-
+		}while(!conferma);
 	}
 }

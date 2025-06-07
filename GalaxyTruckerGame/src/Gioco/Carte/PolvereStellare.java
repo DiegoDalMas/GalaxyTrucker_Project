16e80426d60fe227883f4cc2a9aa058b7194d6e4
DavideMaffeis <d.maffeis10@studenti.unibg.it>
2025-06-07 14:01:08 +0200

    aggioranata spazioAperto che non funzionava per il primo giocatore ma solo per gli altri

--- GalaxyTruckerGame/src/Gioco/Carte/PolvereStellare.java
+++ GalaxyTruckerGame/src/Gioco/Carte/PolvereStellare.java
@@ -1,22 +1,30 @@
 package Gioco.Carte;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import Gioco.Giocatore;
+import Gioco.PlanciaVolo;
 
 public class PolvereStellare extends Carta {
-	public PolvereStellare() {
-
-	}
-
+	
+	//COSTRUTTORE DEFAULT - NO ATTRIBUTI
+	
 	@Override
-	public void applicaEffetto(List<Giocatore> giocatori, Giocatore leader) {
+	public void applicaEffetto(List<Giocatore> giocatori, Giocatore leader, PlanciaVolo plancia){
 		System.out.println("CARTA POLVERE STELLARE");
-		for(int i=giocatori.size();i>0;i--) {
-			int numConnettoriEsposti= giocatori.get(i).getAstronave().connettoriEsposti();
-			giocatori.muoviGiocatore(numConnettoriEsposti); // creare un metodo in Giocatore? che faccia perdere i giorni di volo
+		System.out.println("In ordine INVERSO ogni giocatore perde Giorni di volo per ogni connettore esposto");
+
+
+		//giocatori.reversed() non funzionava
+		List<Giocatore> ordineInvertito = new ArrayList<>(giocatori);
+		Collections.reverse(ordineInvertito);
+		for(Giocatore g: ordineInvertito){
+			int connettoriEsposti = g.getAstronave().connettoriEsposti();
+			System.out.println("Giocatore: " + g.getColore() + " ha " + connettoriEsposti + " connettori esposti");
+			plancia.muoviGiocatore(g.getColore(), -connettoriEsposti);
+			System.out.println("Retrocedi di " + connettoriEsposti + " giorni di volo.");
 		}
-
-
 	}
 }

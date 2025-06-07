51eae593869ec683c125b6e7175fa0533c389323
DavideMaffeis <d.maffeis10@studenti.unibg.it>
2025-06-07 14:43:17 +0200

    class StazioneAbbandonata rifatta perche aveva troppi errori tra cui non cera costruttore, assegnava solo una merce e non piu merci, controllo con nextBoolean bocciato, gestito con random anche questa carta

--- GalaxyTruckerGame/src/Gioco/Carte/StazioneAbbandonata.java
+++ GalaxyTruckerGame/src/Gioco/Carte/StazioneAbbandonata.java
@@ -1,44 +1,71 @@
 package Gioco.Carte;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Random;
 
+import Gioco.Colore;
 import Gioco.Giocatore;
 import Gioco.Merci;
+import Gioco.PlanciaVolo;
 
 public class StazioneAbbandonata extends Carta {
 
-	private boolean conferma;
+	private int equipaggioRichiesto;
+	private int giorniVoloPersi;
+	private int numeroMerci;
+	private List<Merci> ricompensa;
 
-	private Scanner in = new Scanner(System.in);
-	private Merci coloreMerci;
-	private int numeroCarta;
-	private int giorniPersi;
-
-	public StazioneAbbandonata() {
-
+	public StazioneAbbandonata(){
+		Random rand = new Random();
+		this.equipaggioRichiesto = rand.nextInt(2) + 5;	//DA 5 A 6
+		this.giorniVoloPersi = rand.nextInt(2) + 1;
+		this.ricompensa = new ArrayList<>();
+		this.numeroMerci = rand.nextInt(2) + 2;
+		for(int i=0; i<numeroMerci; i++){
+			Colore[] coloriDisponibili = {Colore.ROSSO, Colore.VERDE, Colore.BLU, Colore.GIALLO};
+			int posColore = rand.nextInt(4);
+			Colore coloreScelto = coloriDisponibili[posColore];
+			ricompensa.add(new Merci(coloreScelto));
+		}
 	}
 
 	@Override
-	public void applicaEffetto(List<Giocatore> giocatori, Giocatore leader) {
-		System.out.println("CARTA STAZIONE ABBANDONATA");
-		int i=0;
-		do {
-			System.out.println("giocatore " + giocatori.get(i).getColore() + "vuoi fermarti?"); // richiesta al giocatore se vuole fermarsi
-			conferma = in.nextBoolean();
-			if (conferma == true) {
-				int equipaggio = giocatori.get(i).getAstronave().getEquipaggio();
-				if (equipaggio > numeroCarta) {
-					giocatori.get(i).getAstronave().assegnaMerce(coloreMerci);// prende le merci dalla carta e le assegna al giocatore oppure alla sua
-					//AGGIORNA LEADER     // asrtronave?
-				} else {
-					System.out.print("non hai abbastanza equipaggio");
-					conferma = false;
-				}
-			}
-			i++;
-		} while (conferma == false);
-		giocatori.aggiornaPosizioni(giorniPersi);// aggiornare le posizioni
-	}
+	public void applicaEffetto(List<Giocatore> giocatori, Giocatore leader, PlanciaVolo plancia){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("CARTA STAZIONE SPAZIALE");
+		System.out.println("Equipaggio richiesto: " + equipaggioRichiesto);
+		System.out.println("Merci disponibili: " + ricompensa.size());
+		System.out.print("Colori delle merci: ");
+		for (Merci m : ricompensa) {
+			System.out.print("[" + m.getColore() + "] ");
+		}
+		System.out.println("Giorni di volo persi: " + giorniVoloPersi);
 
+		for(Giocatore g: giocatori){
+			System.out.println("Giocatore: " + g.getColore());
+			System.out.println("Hai un equipaggio di " + g.getAstronave().getEquipaggio());
+
+			if(g.getAstronave().getEquipaggio() < equipaggioRichiesto){
+				System.out.println("Non hai abbastanza equipaggio. Tocca al prossimo giocatore");
+				continue;
+			}
+
+			String scelta;
+			do{
+				System.out.println("Vuoi attraccare alla stazione? (s/n)");
+				scelta = sc.nextLine().toLowerCase();
+			}while(!scelta.equals("s") && !scelta.equals("n"));
+
+			if(scelta.equals("s")){
+				g.getAstronave().assegnaMerce(ricompensa);
+				plancia.muoviGiocatore(g.getColore(), -giorniVoloPersi);
+				System.out.println("Hai perso " + giorniVoloPersi + " giorni di volo");
+				break;
+			}else{
+				System.out.println("Hai rinunciato, tocca al prossimoo giocatore");
+			}
+		}
+	}
 }
